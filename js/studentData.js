395c75f33d69323724e9793cf83ed4b1d6263458
Soorya <sooryatheerth25005@gmail.com>
2026-09-06 11:54:09 +0530

    Add semester information to student data

--- js/studentData.js
+++ js/studentData.js
@@ -3,24 +3,34 @@ const students = [
         id: "ST001",
         name: "Arun Kumar",
         course: "Computer Science",
-        year: 2
+        year: 2,
+        semester: 4
     },
     {
         id: "ST002",
         name: "Meera Nair",
         course: "Computer Science",
-        year: 3
+        year: 3,
+        semester: 6
     },
     {
         id: "ST003",
         name: "Rahul Raj",
         course: "Electronics",
-        year: 2
+        year: 2,
+        semester: 4
     },
     {
         id: "ST004",
         name: "Anjali S",
         course: "Mechanical",
-        year: 1
+        year: 1,
+        semester: 2
     }
 ];
+
+function getActiveStudents() {
+    return students.filter(
+        student => student.year >= 1
+    );
+}
